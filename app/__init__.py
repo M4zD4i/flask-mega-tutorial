06f8ca0c0444b76fc737bceebce065f0901beb00
Alexey Lapaev <a.lapaev@tinkoff.ru>
2018-07-19 22:03:48 +0300

    Chapter 11 - Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -19,6 +20,8 @@ mail = Mail(myapp)
 
 login = LoginManager(myapp)
 login.login_view = 'login'
+
+bootstrap = Bootstrap(myapp)
 
 from app import routes, models, errors
 
