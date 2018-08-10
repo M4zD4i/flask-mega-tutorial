da767b424d46485637d1d9c41a94aad77f19d638
Alexey Lapaev <a.lapaev@tinkoff.ru>
2018-08-10 22:17:06 +0300

    Chapter 16 - Integration with ElasticSearch

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,7 @@ from flask_babel import lazy_gettext as _l
 import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
+from elasticsearch import Elasticsearch
 
 
 db = SQLAlchemy()
@@ -44,6 +45,9 @@ def create_app(config_class=Config):
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+
+    app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+        if app.config['ELASTICSEARCH_URL'] else None
 
     if not app.debug and not app.testing:
         if app.config['MAIL_SERVER']:
